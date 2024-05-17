b653eb60ce84d8689993dd1ea9e769a7434b25d2
eldusastig <144524582+eldusastig@users.noreply.github.com>
2024-05-18 00:20:26 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import tensorflow as tf
 import numpy as np
 from PIL import Image, ImageOps
 
-@st.cache_resource(allow_output_mutation=True)
+@st.cache(allow_output_mutation=True)
 def load_model():
     model = tf.keras.models.load_model('model1.h5')
     return model
