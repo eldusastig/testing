d3048e302c24f59885e96ab6b916c877b5e935b2
eldusastig <144524582+eldusastig@users.noreply.github.com>
2024-05-18 13:46:34 +0800

    Update main.py

--- main.py
+++ main.py
@@ -46,7 +46,7 @@ def main():
         st.write("Welcome to the Malaria Detection app! This app uses a Convolutional Neural Network (CNN) model to classify if a cell is infected with Malaria or not")
         st.write("Upload an image and the app will predict whether an cell is infected or not")
         st.write("Uploaded Image should only contain ONE cell")
-        malaria_banner = "malaria.png"  # Replace this URL with the URL of your image
+        malaria_banner = "https://github.com/eldusastig/testing/blob/main/malaria.png"  # Replace this URL with the URL of your image
         st.image( malaria_banner , caption='Like this ', use_column_width=True)
         test_image = st.file_uploader("Choose an Image:")
         if test_image is not None:
